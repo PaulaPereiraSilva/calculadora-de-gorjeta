44fae0815c7309f0dbe9c7a23bf9d2577e28a0af
Paula Pereira Silva <paula.pereira.ss18@gmail.com>
2025-04-07 10:41:17 -0300

    Iniciando o JavaScript

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
-let Bill = 0
+let bill = 0
 let TipPercentage = 0
 let numberOfPeople = 0
 
-let BillInput = document.querySelector (#bill)
+let BillInput = document.querySelector ("#Bill")
+BillInput.addEventListener("input", reciveBillVallue)
+
+let numberOfPeopleInput = document.querySelector("#People")
+numberOfPeople.addEventListener("input", reciveNumberOfPeopleValue)
+
+function reciveBillVallue(){
+bill = bill.valueasNumber
+caculate()
+}
+
+function reciveNumberOfPeopleValue(){
+numberOfPeople = numberOfPeopleInput.valueasNumber
+caculate()
+}
+
+function caculate(){
+if (bill !== 0 && tipPecentage !== 0 && numberOfPeople !== 0){
+    console.log("Não é possível calcular")
+}
+
+}
