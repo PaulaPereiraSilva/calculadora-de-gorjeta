0e720d260e74f427a6c6118e15405ec2a67b2390
Paula Pereira Silva <paula.pereira.ss18@gmail.com>
2025-04-07 11:21:11 -0300

    javaScript

--- main.js
+++ main.js
@@ -25,8 +25,17 @@ function reciveTipPercentageValue(value){
     buttonSelected = document.querySelector(#button-$(value
     buttonSelected.classList>addEventListener("button-selected")
 
+    document.querySelector("#custom-tip").value =""
+
 )
 }
+
+function reviveCustomTipPercentageValue(){
+tipPecentage = document.querySelector("#custom-tip").valueAsNumber/100
+removeClassButtonSelectted()
+caculate()
+}
+
 function removeClassButtonSelectted(){
     if (buttonSelected !== null){
         buttonSelected.classList.remove("button-selected")
@@ -36,6 +45,17 @@ function removeClassButtonSelectted(){
 function caculate(){
 if (bill !== 0 && tipPecentage !== 0 && numberOfPeople !== 0){
     console.log("Não é possível calcular")
+}
+else{
+    console.log("Não é possível calcular")
+    let tipAmountStrong = querySelector(".amoun strong")
+    let tipAmountPerson = bill * tipPecentage/numberOfPeople
+    tipAmountStrong.innerText = $${tipAmountPerson.toFixed(2)}
+
+    let totalStong = document.querySelector(".total strong")
+    let totalPerson = (bill/numberOfPeople) + tipAmountPerson
+
+    totalStong.innerText = $${totalPerson.toFixed(2)}
 }
 
 }
