5e12876ec734ae0a2b7d08d04ea9cfead11b59de
Paula Pereira Silva <paula.pereira.ss18@gmail.com>
2025-04-07 10:59:51 -0300

    criação do botão selecionar apenas um

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let bill = 0
 let TipPercentage = 0
 let numberOfPeople = 0
+let buttonSelected = null
 
 let BillInput = document.querySelector ("#Bill")
 BillInput.addEventListener("input", reciveBillVallue)
@@ -12,12 +13,26 @@ function reciveBillVallue(){
 bill = bill.valueasNumber
 caculate()
 }
-
 function reciveNumberOfPeopleValue(){
 numberOfPeople = numberOfPeopleInput.valueasNumber
 caculate()
 }
+function reciveTipPercentageValue(value){
+  tipPecentage = value/100
 
+  removeClassButtonSelectted(
+
+    buttonSelected = document.querySelector(#button-$(value
+    buttonSelected.classList>addEventListener("button-selected")
+
+)
+}
+function removeClassButtonSelectted(){
+    if (buttonSelected !== null){
+        buttonSelected.classList.remove("button-selected")
+        buttonSelected = null
+    }
+}
 function caculate(){
 if (bill !== 0 && tipPecentage !== 0 && numberOfPeople !== 0){
     console.log("Não é possível calcular")
